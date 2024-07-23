82aff97f173d2283ef9338decd8c7ab52610d730
T0T0W <161255413+T0T0W@users.noreply.github.com>
2024-07-23 20:55:52 +0200

    avalon chests now appear properly

--- scripts/Drawings/ChestsDrawing.js
+++ scripts/Drawings/ChestsDrawing.js
@@ -8,7 +8,7 @@
       const hX = -1 * chestOne.posX + lpX;
       const hY = chestOne.posY - lpY;
 
-      if (chestOne.hY == 0 && chestOne.hX == 0) {
+      if (chestOne.hX === 0 && chestOne.hY === 0) {
         chestOne.hX = hX;
         chestOne.hY = hY;
       }
@@ -17,41 +17,26 @@
       chestOne.hY = this.lerp(chestOne.hY, hY, t);
     }
   }
+
   invalidate(ctx, chests) {
     for (const chestOne of chests) {
       const point = this.transformPoint(chestOne.hX, chestOne.hY);
-
-      if (
-        this.settings.chestGreen &&
-        (chestOne.chestName.toLowerCase().includes("standard") ||
-          chestOne.chestName.toLowerCase().includes("green"))
-      ) {
-        this.DrawCustomImage(ctx, point.x, point.y, "green", "Resources", 50);
-      } else if (
-        this.settings.chestGreen &&
-        (chestOne.chestName.toLowerCase().includes("uncommon") ||
-          chestOne.chestName.toLowerCase().includes("blue"))
-      ) {
-        this.DrawCustomImage(ctx, point.x, point.y, "blue", "Resources", 50);
-      } else if (
-        this.settings.chestGreen &&
-        (chestOne.chestName.toLowerCase().includes("rare") ||
-          chestOne.chestName.toLowerCase().includes("purple"))
-      ) {
-        this.DrawCustomImage(ctx, point.x, point.y, "rare", "Resources", 50);
-      } else if (
-        this.settings.chestGreen &&
-        (chestOne.chestName.toLowerCase().includes("legendary") ||
-          chestOne.chestName.toLowerCase().includes("yellow"))
-      ) {
-        this.DrawCustomImage(
-          ctx,
-          point.x,
-          point.y,
-          "legendary",
-          "Resources",
-          50,
-        );
+      const chestNameLower = chestOne.chestName.toLowerCase();
+  
+      if (this.settings.chestGreen) {
+        if (chestNameLower.includes("avalon_small")) {
+          this.DrawCustomImage(ctx, point.x, point.y, "avalon_green", "Resources", 35);
+        } else if (chestNameLower.includes("avalon_medium")) {
+          this.DrawCustomImage(ctx, point.x, point.y, "avalon_blue", "Resources", 35);
+        } else if (chestNameLower.includes("standard") || chestNameLower.includes("green")) {
+          this.DrawCustomImage(ctx, point.x, point.y, "green", "Resources", 35);
+        } else if (chestNameLower.includes("uncommon") || chestNameLower.includes("blue")) {
+          this.DrawCustomImage(ctx, point.x, point.y, "blue", "Resources", 35);
+        } else if (chestNameLower.includes("rare") || chestNameLower.includes("purple")) {
+          this.DrawCustomImage(ctx, point.x, point.y, "rare", "Resources", 35);
+        } else if (chestNameLower.includes("legendary") || chestNameLower.includes("yellow")) {
+          this.DrawCustomImage(ctx, point.x, point.y, "legendary", "Resources", 35);
+        }
       }
     }
   }
